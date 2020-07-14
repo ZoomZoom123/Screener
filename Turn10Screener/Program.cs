7089fe1a748f1b36e853c8e4d31035ffa9718b6c
ZoomZoom123 <56050485+ZoomZoom123@users.noreply.github.com>
2020-07-13 18:08:52 -0700

    Solution #1 complete

--- Turn10Screener/Program.cs
+++ Turn10Screener/Program.cs
@@ -4,36 +4,70 @@ using System.Linq;
 
 namespace Turn10Screener
 {
-    public class SumCombinations
+    public class Program
     {
-        public static Int32 Main(string[] args)
+        private static List<int> numbers;
+        private static List<int[]> output_indexes;
+        private static List<int[]> output_numbers;
+        private static int targetSum;
+        private static string[] input;
+        private static List<int> subset;
+        private static List<int> subindexes;
+
+        public static void Main(string[] args)
         {
-            // Get input
+            PrintSumCombinations(numbers, 25);
+        }
+
+        public static int PrintSumCombinations(List<int> numbers, int targetsum)
+        {
+            GetInput();
+            CreateArrayList();
+            SumUpNumbers();
+            CombinationsAndChecks();
+            SolutionOutput();
+            Console.WriteLine("Operation Complete.");
+            //End
+            return 0;
+        }
+
+        private static void GetInput()
+        {
+            // Get input from user
             Console.Write("What number would you like to target? ");
-            int target_sum = Convert.ToInt32(Console.ReadLine());
+            targetSum = Convert.ToInt32(Console.ReadLine());
             Console.Write("What is the list of numbers you would like to use to get to the target number (use spaces)? ");
-            string[] input = Console.ReadLine().Split(' ');
+            input = Console.ReadLine().Split(' ');
+        }
 
+        private static void CreateArrayList()
+        {
             // Create lists
-            List<int> numbers = new List<int>();
-            List<int[]> output_indexes = new List<int[]>();
-            List<int[]> output_numbers = new List<int[]>();
+            numbers = new List<int>();
+            output_indexes = new List<int[]>();
+            output_numbers = new List<int[]>();
+        }
 
+        private static void SumUpNumbers()
+        {
             // Add numbers
             for (int i = 0; i < input.Length; i++)
             {
                 numbers.Add(Convert.ToInt32(input[i]));
             }
+        }
 
-            // Calculate the number of combinations
+        private static void CombinationsAndChecks()
+        {
+            // Calculate the number of possible combinations
             int combinations = (int)(Math.Pow(2, numbers.Count) - 1);
 
             // Loop all combinations
             for (int i = 0; i < combinations; i++)
             {
                 // Create subset lists
-                List<int> subset = new List<int>();
-                List<int> subindexes = new List<int>();
+                subset = new List<int>();
+                subindexes = new List<int>();
 
                 // Loop all numbers
                 for (int j = 0; j < numbers.Count; j++)
@@ -47,27 +81,27 @@ namespace Turn10Screener
                 }
 
                 // Check if the target number has been reached
-                if (subset.Sum() == target_sum)
+                if (subset.Sum() == targetSum)
                 {
                     // Add combination to list
                     output_indexes.Add(subindexes.ToArray());
                     output_numbers.Add(subset.ToArray());
                 }
             }
+        }
 
+        private static void SolutionOutput()
+        {
             // Write output
             Console.WriteLine("\n-----------------------------Output-----------------------------");
 
             // Loop output
             for (int i = 0; i < output_indexes.Count; i++)
             {
-                Console.WriteLine(string.Join(" ", output_indexes[i]) + " (" + string.Join(" + ", output_numbers[i]) + " = " + target_sum.ToString() + ")");
+                Console.WriteLine(string.Join(" ", output_indexes[i]) + " (" + string.Join(" + ", output_numbers[i]) + " = " + targetSum.ToString() + ")");
             }
-            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine("---------------------------------------------------\nPress Enter.");
             Console.ReadKey();
-
-            //End
-            return 0;
-        } 
+        }
     }
-} 
+}
